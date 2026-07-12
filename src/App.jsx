43e7d433e0d4c46a058e897120f6ce5fb92d582b
Martret92 <jaime.martret@gmail.com>
2026-07-12 13:01:34 +0200

    chore: initialize React and Vite project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <section className="presentation" aria-labelledby="app-title">
+        <h1 id="app-title">DevData Generator</h1>
+        <p>Generador configurable de datos ficticios</p>
+        <p>Proyecto inicial preparado con React y Vite</p>
+      </section>
+    </main>
+  )
+}
+
+export default App
